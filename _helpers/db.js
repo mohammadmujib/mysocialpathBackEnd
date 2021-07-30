e65d70c5cfab5263736cf3400ab0c7eb8218ab8c
Mohammad Mujib <63091483+mohammadmujib@users.noreply.github.com>
2021-07-30 20:09:30 +0530

    Update Db.js

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,6 +1,6 @@
 const config = require('config.json');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/users');
+mongoose.connect('mongodb://sami:sami123@localhost/users');
 //Server config
 // mongoose.connect(config.prodURL);
 mongoose.Promise = global.Promise;
